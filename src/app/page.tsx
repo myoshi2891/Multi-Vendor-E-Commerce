a21ffb0086efe1e39dda23a6979bc2c6994c098a
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-28 21:34:47 +0900

    Step-by-Step Guide to Installing Shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
 		<div className="">
 			<h1 className="font-bold">Welcome to the Course</h1>
+			<Button variant="destructive">Click here</Button>
 		</div>
 	);
 }
