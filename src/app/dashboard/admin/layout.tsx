8e64499c0360104dfa15d0cb2f48282f087dbcbe
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-29 15:05:57 +0900

    Creating an Efficient Admin Dashboard Header

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -1,9 +1,25 @@
+import Header from "@/components/dashboard/header/Header";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-export default function AdminDashboardLayout({
+export default async function AdminDashboardLayout({
 	children,
 }: {
 	children: ReactNode;
 }) {
-	return <div>{children}</div>;
+	// Block non admins from accessing the admin dashboard
+	const user = await currentUser();
+
+	if (!user || user.privateMetadata.role !== "ADMIN") redirect("/");
+	return (
+		<div className="w-full h-full">
+			{/* Sidebar */}
+			<div className="w-full ml-[300px]">
+				{/* Header */}
+				<Header />
+				<div className="w-full mt-[75px] p-4">{children}</div>
+			</div>
+		</div>
+	);
 }
