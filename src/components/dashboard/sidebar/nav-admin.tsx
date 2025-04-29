02a64485424b3b894298df887b368c55a057d690
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-29 16:20:26 +0900

    Configuring Admin Navigation for the Dashboard Sidebar

---
+++ src/components/dashboard/sidebar/nav-admin.tsx
@@ -0,0 +1,5 @@
+import { DashboardSidebarMenuInterface } from "@/lib/types";
+
+export default function SideBarNavAdmin({menuLinks}: {menuLinks: DashboardSidebarMenuInterface[]}) {
+  return <div>SideBarNavAdmin</div>;
+}
--- src/components/dashboard/sidebar/sidebar.tsx
+++ src/components/dashboard/sidebar/sidebar.tsx
@@ -2,6 +2,8 @@ import Logo from "@/components/shared/logo";
 import { currentUser } from "@clerk/nextjs/server";
 import { FC } from "react";
 import UserInfo from "./user-info";
+import SideBarNavAdmin from "./nav-admin";
+import { adminDashboardSidebarOptions } from "@/constants/data";
 
 interface SideBarProps {
 	isAdmin?: boolean;
@@ -11,11 +13,14 @@ const Sidebar: FC<SideBarProps> = async ({ isAdmin }) => {
 	const user = await currentUser();
 
 	return (
-        <div className="w-[300px] border-r h-screen p-4 flex flex-col fixed top-0 left-0 bottom-0">
-            <Logo width="100%" height="180px" />
-            <span className="mt-3" />
-            {user && <UserInfo user={user} />}
-        </div>
+		<div className="w-[300px] border-r h-screen p-4 flex flex-col fixed top-0 left-0 bottom-0">
+			<Logo width="100%" height="180px" />
+			<span className="mt-3" />
+			{user && <UserInfo user={user} />}
+			{isAdmin && (
+				<SideBarNavAdmin menuLinks={adminDashboardSidebarOptions} />
+			)}
+		</div>
 	);
 };
 
