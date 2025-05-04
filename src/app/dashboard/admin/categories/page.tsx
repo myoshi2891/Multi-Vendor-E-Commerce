b1cb56e679082050677a4896bc137d8bc1bb78e6
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-05 03:15:13 +0900

    Implementing the create in new tab button

--- src/app/dashboard/admin/categories/page.tsx
+++ src/app/dashboard/admin/categories/page.tsx
@@ -22,6 +22,7 @@ export default async function AdminCategoriesPage() {
 				</>
 			}
 			modalChildren={<CategoryDetails />}
+			newTabLink="/dashboard/admin/categories/new"
 			filterValue="name"
 			data={categories}
 			searchPlaceholder="Search category name ..."
