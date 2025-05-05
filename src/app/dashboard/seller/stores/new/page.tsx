9e5e45b348cc07c7e011712e48c01cfe83597d72
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-05 16:45:13 +0900

    Building the Store Details Form: Part 2

--- src/app/dashboard/seller/stores/new/page.tsx
+++ src/app/dashboard/seller/stores/new/page.tsx
@@ -2,7 +2,7 @@ import StoreDetails from "@/components/dashboard/forms/store-details";
 
 export default function SellerNewStorePage() {
 	return (
-		<div className="">
+		<div className="p-2">
 			<StoreDetails />
 		</div>
 	);
