736bef95c7458242d21ac141b2a44190f3b66bdf
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-06 16:57:00 +0900

    Styling the Images Preview Component

--- src/components/dashboard/shared/images-preview-grid.tsx
+++ src/components/dashboard/shared/images-preview-grid.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 // Import of the image shown where are no images available
 import NoImageImg from "../../../../public/assets/images/no_image_2.png"
-import { cn } from "@/lib/utils";
+import { cn, getGridClassName } from "@/lib/utils";
 
 interface ImagesPreviewGridProps {
 	images: { url: string }[];
@@ -17,6 +17,9 @@ const ImagesPreviewGrid: FC<ImagesPreviewGridProps> = ({
 }) => {
 	// Calculate the number of images
 	let imagesLength = images.length;
+
+	// Get the grid class name based on the number of images
+	const GridClassName = getGridClassName(imagesLength);
 	// If there is no images, display a placeholder image
 	if (imagesLength === 0) {
 		return (
@@ -34,9 +37,23 @@ const ImagesPreviewGrid: FC<ImagesPreviewGridProps> = ({
 		// if there are images, display them in a grid
 		return (
 			<div className="max-x-4xl">
-				<div className={cn("grid grid-cols-2 h-[800px] overflow-hidden bg-white rounded-md")}>
+				<div
+					className={cn(
+						"grid h-[800px] overflow-hidden bg-white rounded-md",
+						GridClassName
+					)}
+				>
 					{images.map((image, index) => (
-						<div key={index} className={cn("relative group h-full w-full border border-gray-300")}>
+						<div
+							key={index}
+							className={cn(
+								"relative group h-full w-full border border-gray-300",
+								`grid_${imagesLength}_image_${index + 1}`,
+								{
+									"h-[266.66px]": images.length === 6,
+								}
+							)}
+						>
 							{/* Image */}
 							<Image
 								src={image.url}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+// Helper function to grid classnames depending on length
+export const getGridClassName = (length: number) => {
+	switch (length) {
+		case 2:
+			return "grid-cols-2";
+		case 3:
+			return "grid-rows-2 grid-cols-2";
+		case 4:
+			return "grid-rows-2 grid-cols-1";
+		case 5:
+			return "grid-rows-2 grid-cols-6";
+		case 6:
+			return "grid-cols-2";
+		default:
+			return "";
+	}
+};
