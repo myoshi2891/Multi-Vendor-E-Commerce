4f341f0a69cd036dd083a85650014232166b7b8f
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-06 15:58:20 +0900

    Creating the Product Form: Part 2

--- src/app/dashboard/seller/stores/[storeUrl]/products/new/page.tsx
+++ src/app/dashboard/seller/stores/[storeUrl]/products/new/page.tsx
@@ -1,10 +1,18 @@
+import ProductDetails from "@/components/dashboard/forms/product-details";
 import { getAllCategories } from "@/queries/category";
 
 export default async function SellerNewProductPage({
 	params,
 }: {
 	params: { storeUrl: string };
-    }) {
-    const categories = await getAllCategories()
-	return <div className="w-full">SellerNewProductPage</div>;
+}) {
+	const categories = await getAllCategories();
+	return (
+		<div className="w-full">
+			<ProductDetails
+				categories={categories}
+				storeUrl={params.storeUrl}
+			/>
+		</div>
+	);
 }
--- src/components/dashboard/shared/image-upload.tsx
+++ src/components/dashboard/shared/image-upload.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 
 // Cloudinary
 import { CldUploadWidget } from "next-cloudinary";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
 
 interface ImageUploadProps {
 	disabled?: boolean;
@@ -128,7 +130,73 @@ const ImageUpload: FC<ImageUploadProps> = ({
 			</div>
 		);
 	} else {
-		<div className=""></div>;
+		return (
+			<div className="">
+				<div className="mb-4 flex items-center gap-4">
+					{value.length > 0 &&
+						!dontShowPreview &&
+						value.map((imageUrl) => (
+							<div
+								key={imageUrl}
+								className="relative w-[200px] min-h-[100px] max-h-[200px]"
+							>
+								{/* Delete image btn */}
+								<div className="z-10 absolute top-2 right-2">
+									<Button
+										onClick={() => onRemove(imageUrl)}
+										type="button"
+										variant="destructive"
+										size="icon"
+										className="rounded-full"
+									>
+										<Trash className="w-4 h-4" />
+									</Button>
+								</div>
+								{/* Image */}
+								<Image
+									fill
+									className="object-cover rounded-md w-full h-full"
+									src={imageUrl}
+									alt=""
+								/>
+							</div>
+						))}
+				</div>
+				<CldUploadWidget uploadPreset="fefik77l" onSuccess={onUpload}>
+					{({ open }) => {
+						const onClick = () => {
+							open();
+						};
+
+						return (
+							<>
+								<button
+									type="button"
+									className="flex items-center font-medium text-[17px] py-3 px-6 gap-x-2 text-white bg-gradient-to-t from-blue-primary to-blue-300 border-none shadow-lg rounded-full hover:shadow-md active:shadow-sm"
+									disabled={disabled}
+									onClick={onClick}
+								>
+									<svg
+										viewBox="0 0 640 512"
+										fill="white"
+										height="1.5em"
+										xmlns="http://www.w3.org/2000/svg"
+										className="mx-auto"
+									>
+										<path d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z" />
+									</svg>
+									<span className="mr-3">
+										{value.length > 0
+											? "Change cover"
+											: "Upload a cover"}
+									</span>
+								</button>
+							</>
+						);
+					}}
+				</CldUploadWidget>
+			</div>
+		);
 	}
 
 	return <div className=""></div>;
