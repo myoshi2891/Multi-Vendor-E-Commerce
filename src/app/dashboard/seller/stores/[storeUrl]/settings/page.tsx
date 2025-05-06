29e482292e26eb0a9e1c2f1d5e54ac967026c691
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-06 12:38:45 +0900

    Setting Up the Store Settings Page

--- src/app/dashboard/seller/stores/[storeUrl]/settings/page.tsx
+++ src/app/dashboard/seller/stores/[storeUrl]/settings/page.tsx
@@ -1,4 +1,22 @@
+// DB
+import { db } from "@/lib/db";
 
-export default function SellerStoreSettingPage() {
-  return <div>Store Setting Page</div>;
+import StoreDetails from "@/components/dashboard/forms/store-details";
+import { redirect } from "next/navigation";
+export default async function SellerStoreSettingPage({
+	params,
+}: {
+	params: { storeUrl: string };
+}) {
+	const storeDetails = await db.store.findUnique({
+		where: {
+			url: params.storeUrl,
+		},
+	});
+	if (!storeDetails) redirect("/dashboard/seller/stores");
+	return (
+		<div className="">
+			<StoreDetails data={storeDetails} />
+		</div>
+	);
 }
