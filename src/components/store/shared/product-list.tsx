871f6e873634b4a20aaad2c40ea437a2d4c8a3f2
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-23 17:22:45 +0900

    Implementing Product List Component

---
+++ src/components/store/shared/product-list.tsx
@@ -0,0 +1,49 @@
+import { ProductType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+import { FC } from "react";
+
+interface Props {
+	products: ProductType[]; // Array of products data
+	title?: string;
+	link?: string;
+	arrow?: boolean;
+}
+
+const ProductList: FC<Props> = ({ products, title, link, arrow }) => {
+	const Title = () => {
+		if (link) {
+			return (
+				<Link href={link} className="h-12">
+					<h2 className="text-main-primary text-xl font-bold">
+						{title}&nbsp;
+						{arrow && <ChevronRight className="w-3 inline-block" />}
+					</h2>
+				</Link>
+			);
+		} else {
+			return (
+				<h2 className="text-main-primary text-xl font-bold">
+					{title}&nbsp;
+					{arrow && <ChevronRight className="w-3 inline-block" />}
+				</h2>
+			);
+		}
+	};
+    return <div className="relative">{title && <Title />}
+        { 
+            products.length > 0 ? <div className={cn("flex flex-wrap -translate-x-5 w-[calc(100%+3rem)] sm:w-[calc(100%+1.5rem)]", {
+                "mt-2": title
+            })}>
+                {
+                    products.map((product) => (
+                        <h1>{product.name}</h1>
+                    ))
+                }
+            </div> : "No Products"
+        }
+    </div>;
+};
+
+export default ProductList;
