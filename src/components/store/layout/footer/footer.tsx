d70950bf8e56d389a70605451b78c93813aa5ac0
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-23 15:42:34 +0900

    Implementing Footer: Links and Rights

--- src/components/store/layout/footer/footer.tsx
+++ src/components/store/layout/footer/footer.tsx
@@ -1,9 +1,20 @@
+import Contact from "./contact";
+import Links from "./links";
 import Newsletter from "./newsletter";
 
 export default function Footer() {
-    return <div className="w-full bg-white">
-        <Newsletter />
-        {/* Footer links */}
-        {/* Rights */}
-  </div>;
+	return (
+		<div className="w-full bg-white">
+			<Newsletter />
+			<div className="max-w-[1430px] mx-auto">
+				<div className="p-5">
+					<div className="grid md:grid-cols-2 md:gap-x-5">
+						<Contact />
+						<Links />
+					</div>
+				</div>
+			</div>
+			{/* Rights */}
+		</div>
+	);
 }
