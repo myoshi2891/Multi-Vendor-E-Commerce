a2b155986e18fcc2cffd2d14e7b821d5ad4e0d09
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-23 14:53:23 +0900

    Implementing Footer: Newsletter Signup

---
+++ src/components/store/layout/footer/footer.tsx
@@ -0,0 +1,9 @@
+import Newsletter from "./newsletter";
+
+export default function Footer() {
+    return <div className="w-full bg-white">
+        <Newsletter />
+        {/* Footer links */}
+        {/* Rights */}
+  </div>;
+}
