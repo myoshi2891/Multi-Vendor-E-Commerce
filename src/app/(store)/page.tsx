65223dda31e99d2694a6d9a6661a9fef3a40d6bc
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-23 17:49:39 +0900

    Implementing Product Filter Update

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -6,7 +6,8 @@ import { UserButton } from "@clerk/nextjs";
 export default async function HomePage() {
 	const productsData = await getProducts();
 	const { products } = productsData;
-
+	console.log("products", products);
+	
 	return (
 		<div className="p-14">
 			<ProductList products={products} title="Products" arrow />
