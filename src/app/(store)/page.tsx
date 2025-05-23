2bea0f255b62342fafebc075ec0e36f046540841
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-23 16:52:47 +0900

    Implementing getFilteredProducts Query (Base)

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,7 +1,10 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
+import { getProducts } from "@/queries/product";
 import { UserButton } from "@clerk/nextjs";
 
-export default async function Home() {
+export default async function HomePage() {
+	const products = await getProducts({}, "");
+
 	return (
 		<div className="p-5">
 			{/* <div className="w-100 flex gap-x-5 justify-end"> */}
