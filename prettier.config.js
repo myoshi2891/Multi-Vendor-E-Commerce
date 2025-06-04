34328b8a6c1f650d07375798abc1de33ed36c2a8
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-06-04 17:05:09 +0900

    Fix: #16 & Creating Custom UI for Review Input and Select

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js
+/** @type {import('prettier').Config} */
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    plugins: ['prettier-plugin-tailwindcss']
+}
