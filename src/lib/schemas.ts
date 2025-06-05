70e7b2eafec43204d20eef6d478c41cdcdc8feca
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-06-05 20:59:52 +0900

    Product Form: Input Fieldset and Styling Fixes

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -213,6 +213,13 @@ export const ProductFormSchema = z.object({
             invalid_type_error: 'Product subcategory must be a string',
         })
         .uuid(),
+    offerTagId: z
+        .string({
+            required_error: 'Product offer tag ID is mandatory.',
+            invalid_type_error: 'Product offer tag ID must be a valid UUID.',
+        })
+        .uuid()
+        .optional(),
     brand: z
         .string({
             required_error: 'Product brand is required',
