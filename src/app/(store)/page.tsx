0006b82c1192c17d817f39f856ce833bdfc374cc
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-06-08 20:50:48 +0900

    Implementing Remaining Cart Functions

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,16 +1,19 @@
-import ThemeToggle from "@/components/shared/theme-toggle";
-import ProductList from "@/components/store/shared/product-list";
-import { getProducts } from "@/queries/product";
-import { UserButton } from "@clerk/nextjs";
+import { useCartStore } from '@/cart-store/useCartStore'
+import ThemeToggle from '@/components/shared/theme-toggle'
+import ProductList from '@/components/store/shared/product-list'
+import useFromStore from '@/hooks/useFromStore'
+import { getProducts } from '@/queries/product'
+import { UserButton } from '@clerk/nextjs'
 
 export default async function HomePage() {
-	const productsData = await getProducts();
-	const { products } = productsData;
-	console.log("products", products);
-	
-	return (
-		<div className="p-14">
-			<ProductList products={products} title="Products" arrow />
-		</div>
-	);
+    const productsData = await getProducts()
+    const { products } = productsData
+    const cart = useFromStore(useCartStore, (state) => state.cart)
+    const addToCart = useCartStore((state) => state.addToCart)
+
+    return (
+        <div className="p-14">
+            <ProductList products={products} title="Products" arrow />
+        </div>
+    )
 }
