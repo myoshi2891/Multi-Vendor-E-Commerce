a31bc87aa5b4aab3df87458b21536934902fd2e4
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-06-10 21:11:35 +0900

    Implementing the saveUserCart Function to Persist Cart Data: Part 1

--- src/queries/user.ts
+++ src/queries/user.ts
@@ -1,7 +1,8 @@
-"use server";
+'use server'
 
-import { db } from "@/lib/db";
-import { currentUser } from "@clerk/nextjs/server";
+import { db } from '@/lib/db'
+import { CartProductType } from '@/lib/types'
+import { currentUser } from '@clerk/nextjs/server'
 
 /**
  * @name followStore
@@ -12,21 +13,21 @@ import { currentUser } from "@clerk/nextjs/server";
  * @param storeId - The ID of the store to be followed or unfollowed.
  * @returns {boolean} - Returns true if the follow status was updated successfully, false otherwise.
  */
-export const followStore = async (storeId: string): Promise<boolean> => { 
+export const followStore = async (storeId: string): Promise<boolean> => {
     try {
         // Get the current authenticated user
-        const user = await currentUser();
-        
+        const user = await currentUser()
+
         // Ensure user is authenticated
-        if (!user) throw new Error("Unauthenticated.");
+        if (!user) throw new Error('Unauthenticated.')
 
         // Check if the store exists
-        const store = await db.store.findUnique({ where: { id: storeId } });
-        if (!store) throw new Error("Store not found.");  // Store does not exist, cannot follow or unfollow
+        const store = await db.store.findUnique({ where: { id: storeId } })
+        if (!store) throw new Error('Store not found.') // Store does not exist, cannot follow or unfollow
 
         // Check if the user exists
-        const userData = await db.user.findUnique({ where: { id: user.id } });
-        if (!userData) throw new Error("User not found.");  // User does not exist, cannot follow or unfollow
+        const userData = await db.user.findUnique({ where: { id: user.id } })
+        if (!userData) throw new Error('User not found.') // User does not exist, cannot follow or unfollow
 
         // Check if the user is already following the store
         const userFollowingStore = await db.user.findFirst({
@@ -35,41 +36,144 @@ export const followStore = async (storeId: string): Promise<boolean> => {
                 following: {
                     some: {
                         id: storeId,
-                    }
-                }
-            }
+                    },
+                },
+            },
         })
 
         if (userFollowingStore) {
             // Unfollow the store and return false
             await db.store.update({
                 where: {
-                    id: storeId
+                    id: storeId,
                 },
                 data: {
                     followers: {
-                        disconnect: {id: userData.id}
-                    }
-                }
+                        disconnect: { id: userData.id },
+                    },
+                },
             })
-            return false;
+            return false
         } else {
             // Follow the store and return true
             await db.store.update({
                 where: {
-                    id: storeId
+                    id: storeId,
                 },
                 data: {
                     followers: {
-                        connect: {id: userData.id}
-                    }
-                }
+                        connect: { id: userData.id },
+                    },
+                },
             })
-            return true;  // Follow status updated successfully
+            return true // Follow status updated successfully
         }
-
-     } catch (error) { 
-        console.error("Error following store", error); 
-        throw new Error("Error following store");
+    } catch (error) {
+        console.error('Error following store', error)
+        throw new Error('Error following store')
     }
+}
+
+/**
+ * @Function saveUserCart
+ * @Description Saves the user's cart by validating product data from the database and ensuring no frontend manipulation.
+ * @PermissionLevel User who owns the cart
+ * @Parameters
+ * - cartProducts: An array of product objects from the frontend cart.
+ * @Returns {boolean}
+ * - An object containing the updated cart with recalculated total price and validated product data.
+ */
+
+export const saveUserCart = async (
+    cartProducts: CartProductType[]
+): Promise<boolean> => {
+    // Get current user
+    const user = await currentUser()
+
+    // Ensure user is authenticated
+    if (!user) throw new Error('Unauthenticated.')
+
+    const userId = user.id
+
+    // Search for existing user cart
+    const userCart = await db.cart.findFirst({
+        where: {
+            userId,
+        },
+    })
+
+    // Delete any existing user cart
+    if (userCart) {
+        await db.cart.delete({
+            where: {
+                userId,
+            },
+        })
+    }
+
+    // Fetch product, variant, and size data from the database for validation
+    const validatedCartItems = await Promise.all(
+        cartProducts.map(async (cartProduct) => {
+            const { productId, variantId, sizeId, quantity } = cartProduct
+
+            // Fetch the product, variant, and size from the database
+            const product = await db.product.findUnique({
+                where: {
+                    id: productId,
+                },
+                include: {
+                    store: true,
+                    variants: {
+                        where: {
+                            id: variantId,
+                        },
+                        include: {
+                            sizes: {
+                                where: {
+                                    id: sizeId,
+                                },
+                            },
+                            images: true,
+                        },
+                    },
+                },
+            })
+
+            if (
+                !product ||
+                product.variants.length === 0 ||
+                product.variants[0].sizes.length === 0
+            ) {
+                throw new Error(
+                    `Invalid product, variant, or size combination for productId ${productId}, variantId ${variantId}, sizeId ${sizeId}`
+                )
+            }
+
+            const variant = product.variants[0]
+            const size = variant.sizes[0]
+
+            // Validate stock and price
+            const validQuantity = Math.min(quantity, size.quantity)
+            const price = size.discount
+                ? size.price - size.price * (size.discount / 100)
+                : size.price
+
+            return {
+                productId,
+                variantId,
+                productSlug: product.slug,
+                variantSlug: variant.slug,
+                sizeId,
+                storeId: product.storeId,
+                sku: variant.sku,
+                name: `${product.name} ・ ${variant.variantName}`,
+                image: variant.images[0].url,
+                size: size.size,
+                quantity: validQuantity,
+                price,
+                shippingFee: 0,
+                totalPrice: 0,
+            }
+        })
+    )
 }
