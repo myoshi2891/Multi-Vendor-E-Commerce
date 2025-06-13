8f96388cbfea31cea76d637e53341f5e349587a4
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-06-13 18:58:38 +0900

    Creating a Shipping Address Form: Part 2

--- src/components/store/shared/shipping-addresses/address-details.tsx
+++ src/components/store/shared/shipping-addresses/address-details.tsx
@@ -15,6 +15,7 @@ import * as z from 'zod'
 import { ShippingAddressSchema } from '@/lib/schemas'
 
 // UI Components
+import CountrySelector from '@/components/shared/country-selector'
 import {
     Form,
     FormControl,
@@ -23,7 +24,7 @@ import {
     FormLabel,
     FormMessage,
 } from '@/components/ui/form'
-
+import { Button } from '../../ui/button'
 // import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
@@ -37,8 +38,6 @@ import { useRouter } from 'next/navigation'
 
 // types
 import { SelectMenuOption, UserShippingAddressType } from '@/lib/types'
-import { Button } from '../../ui/button'
-import CountrySelector from '@/components/shared/country-selector'
 
 interface AddressDetailsProps {
     data?: UserShippingAddressType
@@ -46,7 +45,11 @@ interface AddressDetailsProps {
     setShow: Dispatch<SetStateAction<boolean>>
 }
 
-const AddressDetails: FC<AddressDetailsProps> = ({ data, countries, setShow }) => {
+const AddressDetails: FC<AddressDetailsProps> = ({
+    data,
+    countries,
+    setShow,
+}) => {
     // Initializing necessary hooks
     const { toast } = useToast() // Hook for displaying toast messages
     const router = useRouter() // Hook for routing
@@ -75,7 +78,7 @@ const AddressDetails: FC<AddressDetailsProps> = ({ data, countries, setShow }) =
             default: data?.default ?? false,
         },
     })
-    
+
     // Loading status based on form submission
     const isLoading = form.formState.isSubmitting
 
@@ -128,8 +131,8 @@ const AddressDetails: FC<AddressDetailsProps> = ({ data, countries, setShow }) =
         setCountry(name)
     }
 
-    console.log("country", form.watch().countryId);
-    
+    console.log('country', form.watch().countryId)
+
     return (
         <div>
             <Form {...form}>
@@ -139,7 +142,7 @@ const AddressDetails: FC<AddressDetailsProps> = ({ data, countries, setShow }) =
                 >
                     <div className="space-y-2">
                         <FormLabel>Contact information</FormLabel>
-                        <div className="flex items-center justify-between gap-4">
+                        <div className="flex items-center justify-between gap-3">
                             <FormField
                                 control={form.control}
                                 name="firstName"
@@ -175,7 +178,7 @@ const AddressDetails: FC<AddressDetailsProps> = ({ data, countries, setShow }) =
                             control={form.control}
                             name="phone"
                             render={({ field }) => (
-                                <FormItem className="!mt-6 w-[calc(50%-8px)] flex-1">
+                                <FormItem className="!mt-3 w-[calc(50%-8px)] flex-1">
                                     <FormControl>
                                         <Input
                                             placeholder="Phone number"
@@ -194,7 +197,7 @@ const AddressDetails: FC<AddressDetailsProps> = ({ data, countries, setShow }) =
                                 control={form.control}
                                 name="countryId"
                                 render={({ field }) => (
-                                    <FormItem className="!mt-6 w-[calc(50%-8px)] flex-1">
+                                    <FormItem className="!mt-3 w-[calc(50%-8px)] flex-1">
                                         <FormControl>
                                             <CountrySelector
                                                 id={'countries'}
@@ -219,8 +222,91 @@ const AddressDetails: FC<AddressDetailsProps> = ({ data, countries, setShow }) =
                                 )}
                             />
                         </div>
+                        <div className="!mt-3 flex items-center justify-between gap-3">
+                            <FormField
+                                control={form.control}
+                                name="address1"
+                                render={({ field }) => (
+                                    <FormItem className="flex-1">
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Street, house/apartment/unit"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="address2"
+                                render={({ field }) => (
+                                    <FormItem className="flex-1">
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Apt, suite, unit, etc (optional)"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className="!mt-3 flex items-center justify-between gap-3">
+                            <FormField
+                                control={form.control}
+                                name="city"
+                                render={({ field }) => (
+                                    <FormItem className="flex-1">
+                                        <FormControl>
+                                            <Input
+                                                placeholder="City"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="state"
+                                render={({ field }) => (
+                                    <FormItem className="flex-1">
+                                        <FormControl>
+                                            <Input
+                                                placeholder="State/Province"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <FormField
+                            control={form.control}
+                            name="zip_code"
+                            render={({ field }) => (
+                                <FormItem className="!mt-3 w-[calc(50%-8px)] flex-1">
+                                    <FormControl>
+                                        <Input
+                                            placeholder="Zip code"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
                     </div>
-                    <Button type="submit" disabled={isLoading}>
+                    <Button
+                        type="submit"
+                        disabled={isLoading}
+                        className="rounded-md"
+                    >
                         {isLoading
                             ? 'loading...'
                             : data?.id
