a2c875ee1b91cf12e8ff591ecdcedb2f58e1e9e2
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-06-19 15:46:17 +0900

    Handling Out of Stock Products in the Cart

--- src/components/store/cards/cart-product.tsx
+++ src/components/store/cards/cart-product.tsx
@@ -90,9 +90,11 @@ const CartProduct: FC<Props> = ({
         }
 
         // Subtract the previous shipping total for this product before updating
-        setTotalShipping(
-            (prevTotal) => prevTotal - shippingInfo.totalFee + totalFee
-        )
+        if (stock > 0) {
+            setTotalShipping(
+                (prevTotal) => prevTotal - shippingInfo.totalFee + totalFee
+            )
+        }
 
         // Update state
         setShippingInfo({
@@ -121,7 +123,7 @@ const CartProduct: FC<Props> = ({
         if (!shippingInfo.totalFee) {
             calculateShipping()
         }
-    }, [quantity, shippingFee, userCountry, shippingInfo.totalFee])
+    }, [quantity, shippingFee, userCountry, shippingInfo.totalFee, stock])
 
     const selected = selectedItems.find(
         (p) => unique_id === `${p.productId}-${p.variantId}-${p.sizeId}`
@@ -160,39 +162,48 @@ const CartProduct: FC<Props> = ({
     }
 
     return (
-        <div className="select-none border-t border-t-[#ebebeb] bg-white px-6">
+        <div
+            className={cn(
+                'select-none border-t border-t-[#ebebeb] bg-white px-6',
+                {
+                    'bg-red-100': stock === 0,
+                }
+            )}
+        >
             <div className="py-4">
                 <div className="relative flex self-start">
                     {/* Image */}
                     <div className="flex items-center">
-                        <label
-                            htmlFor={unique_id}
-                            className="mr-2 inline-flex cursor-pointer items-center p-0 align-middle text-sm leading-6 text-gray-900"
-                        >
-                            <span className="inline-flex cursor-pointer p-0.5 leading-8">
-                                <span
-                                    className={cn(
-                                        'bg-full flex size-5 items-center justify-center rounded-full border border-gray-300 leading-8 hover:border-orange-background',
-                                        {
-                                            'border-orange-background':
-                                                selected,
-                                        }
-                                    )}
-                                >
-                                    {selected && (
-                                        <span className="flex size-5 items-center justify-center rounded-full bg-orange-background">
-                                            <Check className="mt-0.5 w-3.5 text-white" />
-                                        </span>
-                                    )}
+                        {stock > 0 && (
+                            <label
+                                htmlFor={unique_id}
+                                className="mr-2 inline-flex cursor-pointer items-center p-0 align-middle text-sm leading-6 text-gray-900"
+                            >
+                                <span className="inline-flex cursor-pointer p-0.5 leading-8">
+                                    <span
+                                        className={cn(
+                                            'bg-full flex size-5 items-center justify-center rounded-full border border-gray-300 leading-8 hover:border-orange-background',
+                                            {
+                                                'border-orange-background':
+                                                    selected,
+                                            }
+                                        )}
+                                    >
+                                        {selected && (
+                                            <span className="flex size-5 items-center justify-center rounded-full bg-orange-background">
+                                                <Check className="mt-0.5 w-3.5 text-white" />
+                                            </span>
+                                        )}
+                                    </span>
                                 </span>
-                            </span>
-                            <input
-                                type="checkbox"
-                                id={unique_id}
-                                hidden
-                                onChange={() => handleSelectProduct()}
-                            />
-                        </label>
+                                <input
+                                    type="checkbox"
+                                    id={unique_id}
+                                    hidden
+                                    onChange={() => handleSelectProduct()}
+                                />
+                            </label>
+                        )}
                         <Link
                             href={`/product/${productSlug}/${variantSlug}?size=${sizeId}`}
                         >
@@ -244,12 +255,20 @@ const CartProduct: FC<Props> = ({
                         </div>
                         {/* Price - Delivery */}
                         <div className="relative mt-2 flex items-center justify-between">
-                            <div>
-                                <span className="inline-block break-all">
-                                    ${price.toFixed(2)} x {quantity} = $
-                                    {totalPrice.toFixed(2)}
-                                </span>
-                            </div>
+                            {stock > 0 ? (
+                                <div>
+                                    <span className="inline-block break-all">
+                                        ${price.toFixed(2)} x {quantity} = $
+                                        {totalPrice.toFixed(2)}
+                                    </span>
+                                </div>
+                            ) : (
+                                <div>
+                                    <span className="inline-block break-all text-sm text-red-500">
+                                        Out of stock
+                                    </span>
+                                </div>
+                            )}
                             {/* Quantity changer */}
                             <div className="text-xs">
                                 <div className="inline-flex list-none items-center text-sm leading-6 text-gray-900">
@@ -269,7 +288,7 @@ const CartProduct: FC<Props> = ({
                                         readOnly
                                         min={1}
                                         max={stock}
-                                        className="m-1 h-6 w-[32px] border-none bg-white text-center font-bold leading-6 tracking-normal text-gray-900 outline-none"
+                                        className="m-1 h-6 w-[32px] border-none bg-transparent text-center font-bold leading-6 tracking-normal text-gray-900 outline-none"
                                     />
                                     <div
                                         className="grid size-6 cursor-pointer place-items-center rounded-full bg-gray-100 text-xs leading-6 hover:bg-gray-200"
@@ -283,56 +302,62 @@ const CartProduct: FC<Props> = ({
                             </div>
                         </div>
                         {/* Shipping info */}
-                        <div className="mt-1 cursor-pointer text-xs text-[#999]">
-                            <div className="mb-1 flex items-center">
-                                <span>
-                                    <Truck className="inline-block w-4 text-[#01A971]" />
-                                    {shippingInfo.totalFee > 0 ? (
-                                        <span className="ml-1 text-[#01A971]">
-                                            {shippingMethod === 'ITEM' ? (
-                                                <>
-                                                    ${shippingInfo.initialFee}
-                                                    (first item){' '}
-                                                    {quantity === 1
-                                                        ? ''
-                                                        : `+ ${quantity - 1 === 1 ? '1 item' : `${quantity - 1} items`}
+                        {stock > 0 && (
+                            <div className="mt-1 cursor-pointer text-xs text-[#999]">
+                                <div className="mb-1 flex items-center">
+                                    <span>
+                                        <Truck className="inline-block w-4 text-[#01A971]" />
+                                        {shippingInfo.totalFee > 0 ? (
+                                            <span className="ml-1 text-[#01A971]">
+                                                {shippingMethod === 'ITEM' ? (
+                                                    <>
+                                                        $
+                                                        {
+                                                            shippingInfo.initialFee
+                                                        }
+                                                        (first item){' '}
+                                                        {quantity === 1
+                                                            ? ''
+                                                            : `+ ${quantity - 1 === 1 ? '1 item' : `${quantity - 1} items`}
                                                     x ${extraShippingFee}
                                                     (${quantity - 1 === 1 ? '1 additional item' : `${quantity - 1} additional items`})`}
-                                                    = $
-                                                    {shippingInfo.totalFee.toFixed(
-                                                        2
-                                                    )}
-                                                </>
-                                            ) : shippingMethod === 'WEIGHT' ? (
-                                                <>
-                                                    ${shippingFee} x{' '}
-                                                    {shippingInfo.weight}kg x{' '}
-                                                    {quantity}{' '}
-                                                    {quantity > 1
-                                                        ? 'items'
-                                                        : 'item'}{' '}
-                                                    = $
-                                                    {shippingInfo.totalFee.toFixed(
-                                                        2
-                                                    )}
-                                                </>
-                                            ) : (
-                                                <>
-                                                    Fixed Fee : $
-                                                    {shippingInfo.totalFee.toFixed(
-                                                        2
-                                                    )}
-                                                </>
-                                            )}
-                                        </span>
-                                    ) : (
-                                        <span className="ml-1 text-[#01A971]">
-                                            Free Delivery
-                                        </span>
-                                    )}
-                                </span>
+                                                        = $
+                                                        {shippingInfo.totalFee.toFixed(
+                                                            2
+                                                        )}
+                                                    </>
+                                                ) : shippingMethod ===
+                                                  'WEIGHT' ? (
+                                                    <>
+                                                        ${shippingFee} x{' '}
+                                                        {shippingInfo.weight}kg
+                                                        x {quantity}{' '}
+                                                        {quantity > 1
+                                                            ? 'items'
+                                                            : 'item'}{' '}
+                                                        = $
+                                                        {shippingInfo.totalFee.toFixed(
+                                                            2
+                                                        )}
+                                                    </>
+                                                ) : (
+                                                    <>
+                                                        Fixed Fee : $
+                                                        {shippingInfo.totalFee.toFixed(
+                                                            2
+                                                        )}
+                                                    </>
+                                                )}
+                                            </span>
+                                        ) : (
+                                            <span className="ml-1 text-[#01A971]">
+                                                Free Delivery
+                                            </span>
+                                        )}
+                                    </span>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
