682ea1a235437673048561539c9136b2d12ef37d
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-06-25 19:22:59 +0900

    Order - Place Order Query Update

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 // prettier.config.js
 /** @type {import('prettier').Config} */
 module.exports = {
-    semi: false,
-    singleQuote: true,
+    semi: true,
+    singleQuote: false,
     tabWidth: 4,
     trailingComma: 'es5',
     plugins: ['prettier-plugin-tailwindcss'],
