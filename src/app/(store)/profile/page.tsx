8aff02e510c5ea611f62ab4b625ec65c707fa7b0
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-07-05 20:49:19 +0900

    256. Profile Overview Setup

--- src/app/(store)/profile/page.tsx
+++ src/app/(store)/profile/page.tsx
@@ -1,4 +1,9 @@
+import ProfileOverview from "@/components/store/profile/overview";
 
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+    return (
+        <div className="w-full space-y-4">
+            <ProfileOverview />
+        </div>
+    );
 }
