018c43bb6c531254effdd36d903239af39ef14b2
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-07-11 21:22:48 +0900

    268. Manage and Display User Addresses on Profile

--- src/app/(store)/profile/addresses/page.tsx
+++ src/app/(store)/profile/addresses/page.tsx
@@ -1,4 +1,13 @@
+import AddressContainer from "@/components/store/profile/addresses/container";
+import { db } from "@/lib/db";
+import { getUserShippingAddresses } from "@/queries/user";
 
-export default function ProfileAddressesPage() {
-  return <div>ProfileAddressesPage</div>;
+export default async function ProfileAddressesPage() {
+    const addresses = await getUserShippingAddresses();
+    const countries = await db.country.findMany();
+    return (
+        <div>
+            <AddressContainer addresses={addresses} countries={countries} />
+        </div>
+    );
 }
