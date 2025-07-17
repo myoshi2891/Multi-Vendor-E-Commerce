440b826f4b3ec678377f8254942d9ddc326a350f
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-07-18 01:47:53 +0900

    279. Set Up Filters and Category Link Filter with Subcategories for Browse Page

--- src/components/store/browse-page/filters/category/category-filter.tsx
+++ src/components/store/browse-page/filters/category/category-filter.tsx
@@ -3,31 +3,40 @@ import { CategoryWithSubsType } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { Minus, Plus } from "lucide-react";
 import { useState } from "react";
+import CategoryLink from "./category-link";
 
 export default function CategoryFilter({
     categories,
 }: {
     categories: CategoryWithSubsType[];
-    }) {
-    const [show, setShow] = useState<boolean>(false);
+}) {
+    const [show, setShow] = useState<boolean>(true);
     return (
         <div className="pb-4 pt-5">
             {/* Header */}
-            <div className="relative flex cursor-pointer select-none items-center justify-between"
-            onClick={() => setShow(prev => !prev)}>
+            <div
+                className="relative flex cursor-pointer select-none items-center justify-between"
+                onClick={() => setShow((prev) => !prev)}
+            >
                 <h3 className="line-clamp-1 text-ellipsis text-sm font-bold capitalize text-main-primary">
                     Category
                 </h3>
                 <span className="absolute right-0">
-                    {show ? <Minus className="w-3"/> : <Plus className="w-3"/>}
+                    {show ? (
+                        <Minus className="w-3" />
+                    ) : (
+                        <Plus className="w-3" />
+                    )}
                 </span>
             </div>
             {/* Filter */}
-            <div className={cn("mt-2.5", {
-                hidden: !show
-            })}>
+            <div
+                className={cn("mt-2.5", {
+                    hidden: !show,
+                })}
+            >
                 {categories.map((category) => (
-                    <div key={category.id}>{ category.name}</div>
+                    <CategoryLink key={category.id} category={category} />
                 ))}
             </div>
         </div>
