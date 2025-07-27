6e056ca33f559eab1b893226d9d5fbd2f90f3f91
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-07-27 23:09:36 +0900

    293. Seller Dashboard: Orders Table - StoreOrderSummary Info

--- src/components/dashboard/shared/store-order-summary.tsx
+++ src/components/dashboard/shared/store-order-summary.tsx
@@ -1,4 +1,6 @@
+import PaymentStatusTag from "@/components/shared/payment-status";
 import {
+    OrderStatus,
     PaymentStatus,
     StoreOrderType,
     UserShippingAddressType,
@@ -6,17 +8,16 @@ import {
 import { getShippingDatesRange } from "@/lib/utils";
 import { PaymentDetails } from "@prisma/client";
 import { FC } from "react";
+import OrderStatusSelect from "../forms/order-status-select";
 
 interface Props {
     group: StoreOrderType;
 }
 
-const StoreOrderSummary: FC<Props> = ({
-    group,
-}) => {
-    const paymentDetails = group.order.paymentDetails
-    const paymentStatus = group.order.paymentStatus
-    const shippingAddress = group.order.shippingAddress
+const StoreOrderSummary: FC<Props> = ({ group }) => {
+    const paymentDetails = group.order.paymentDetails;
+    const paymentStatus = group.order.paymentStatus as PaymentStatus;
+    const shippingAddress = group.order.shippingAddress;
     const { minDate, maxDate } = getShippingDatesRange(
         group.shippingDeliveryMin,
         group.shippingDeliveryMax,
@@ -53,6 +54,75 @@ const StoreOrderSummary: FC<Props> = ({
                     <h6 className="text-2xl font-semibold leading-9">
                         #{group.id}
                     </h6>
+                    <div className="flex items-center gap-x-2">
+                        <PaymentStatusTag status={paymentStatus} />
+                        <OrderStatusSelect
+                            storeId={group.storeId}
+                            groupId={group.id}
+                            status={group.status as OrderStatus}
+                        />
+                    </div>
+                </div>
+                <div className="mb-6 mt-3 grid grid-cols-1 gap-3 border-gray-100 py-4">
+                    <div className="grid grid-cols-2">
+                        {/* Shipping Service */}
+                        <div>
+                            <p className="mb-3 text-base font-normal leading-7 text-gray-500 transition-all duration-500">
+                                Shipping Service
+                            </p>
+                            <h6 className="text-lg font-semibold leading-9">
+                                {group.shippingService}
+                            </h6>
+                        </div>
+                        {/* Expected Delivery Date */}
+                        <div>
+                            <p className="mb-3 text-base font-normal leading-7 text-gray-500 transition-all duration-500">
+                                Expected Delivery Date
+                            </p>
+                            <h6 className="text-lg font-semibold leading-9">
+                                {minDate} - {maxDate}
+                            </h6>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        {/* Payment Method */}
+                        <div>
+                            <p className="mb-3 text-base font-normal leading-7 text-gray-500 transition-all duration-500">
+                                Payment Method
+                            </p>
+                            <h6 className="text-lg font-semibold leading-9">
+                                {paymentDetails?.paymentMethod || "-"}
+                            </h6>
+                        </div>
+                        {/* Payment Reference */}
+                        <div>
+                            <p className="mb-3 text-base font-normal leading-7 text-gray-500 transition-all duration-500">
+                                Payment Reference
+                            </p>
+                            <h6 className="text-lg font-semibold leading-9">
+                                {paymentDetails?.paymentIntentId || "-"}
+                            </h6>
+                        </div>
+                    </div>
+                    {/* Address */}
+                    <div>
+                        <p className="mb-3 text-base font-normal leading-7 text-gray-500 transition-all duration-500">
+                            Address
+                        </p>
+                        <h6 className="text-lg font-semibold leading-9">
+                            {address1}, {address2 && `${address2},`} {city},
+                            {state}, {zip_code}, {country.name}
+                        </h6>
+                    </div>
+                    {/* Customer */}
+                    <div>
+                        <p className="mb-3 text-base font-normal leading-7 text-gray-500 transition-all duration-500">
+                            Customer
+                        </p>
+                        <h6 className="text-lg font-semibold leading-9">
+                            {firstName} {lastName}, {phone}, {user.email}
+                        </h6>
+                    </div>
                 </div>
             </div>
         </div>
