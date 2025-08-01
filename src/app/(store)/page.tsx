4c4f72545a808b12f3a6fc55a763f74bbdbcb5f5
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-08-02 01:10:35 +0900

    298. Home Page - Finishing Function

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,19 +1,30 @@
 import CategoriesHeader from '@/components/store/layout/categories-header/categories-header'
 import StoreHeader from '@/components/store/layout/header/header'
 import ProductList from '@/components/store/shared/product-list'
-import { getProducts } from '@/queries/product'
+import { getHomeDataDynamic } from "@/queries/home";
+import { getProducts } from "@/queries/product";
 
 export default async function HomePage() {
-    const productsData = await getProducts()
-    const { products } = productsData
+    const productsData = await getProducts();
+    const { products } = productsData;
+
+    const { products_Seasonal } = await getHomeDataDynamic([
+        { property: "offer", value: "Seasonal", type: "full" },
+    ]);
+
+    console.log(products_Seasonal);
 
     return (
         <div>
             <StoreHeader />
             <CategoriesHeader />
             <div className="p-14">
-                <ProductList products={products} title="Products" arrow />
+                <ProductList
+                    products={products_Seasonal}
+                    title="Products"
+                    arrow
+                />
             </div>
         </div>
-    )
+    );
 }
