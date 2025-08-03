42358f73ae1d07c71ccdb1e5649fe233fb6aa702
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-08-03 17:01:57 +0900

    301. Home Page - Main Structure (Left Ad Image and HomeMainSwiper)

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,3 +1,4 @@
+import HomeMainSwiper from "@/components/store/home/main/home-swiper";
 import Sideline from "@/components/store/home/sideline/sideline";
 import CategoriesHeader from "@/components/store/layout/categories-header/categories-header";
 import Footer from "@/components/store/layout/footer/footer";
@@ -5,6 +6,7 @@ import StoreHeader from "@/components/store/layout/header/header";
 import ProductList from "@/components/store/shared/product-list";
 import { getHomeDataDynamic, getHomeFeaturedCategories } from "@/queries/home";
 import { getProducts } from "@/queries/product";
+import Link from "next/link";
 
 export default async function HomePage() {
     const productsData = await getProducts();
@@ -22,6 +24,31 @@ export default async function HomePage() {
             <CategoriesHeader />
             <div className="relative min-h-screen w-full">
                 <Sideline />
+                <div className="relative h-full w-[calc(100%-40px)] bg-[#e3e3e3]">
+                    <div className="mx-auto min-h-screen max-w-[1600px] p-4">
+                        {/* Main */}
+                        <div className="grid w-full gap-2 min-[1170px]:grid-cols-[1fr_350px] min-[1465px]:grid-cols-[200px_1fr_350px]">
+                            {/* Left */}
+                            <Link href="">
+                                <div
+                                    className="hidden h-[555px] cursor-pointer rounded-md bg-cover bg-no-repeat min-[1465px]:block"
+                                    style={{
+                                        backgroundImage:
+                                            "url(/assets/images/ads/winter-sports-clothing.jpg)",
+                                    }}
+                                />
+                            </Link>
+                            {/* Middle */}
+                            <div className="h-fit space-y-2">
+                                {/* Main swiper */}
+                                <HomeMainSwiper />
+                                {/* Featured card */}
+                                <div className="h-[200px]"></div>
+                            </div>
+                            {/* Right */}
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
