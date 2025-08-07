c7c37c9c0b68c11576c9ab8b7817c2f93dff8739
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-08-07 15:29:22 +0900

    314. Apply Seller - Minimal Header

---
+++ src/app/(store)/seller/apply/page.tsx
@@ -0,0 +1,7 @@
+import MinimalHeader from "@/components/store/layout/minimal-header/header";
+
+export default function SellerApplyPage() {
+    return <div className="h-screen overflow-y-hidden bg-[#eef4fc]">
+        <MinimalHeader />
+    </div>;
+}
