1505a11ca71adbc01d2a49e83fb195641d684150
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-08-08 18:09:27 +0900

    315. Apply Seller - Form Setup with Instructions

---
+++ src/components/store/forms/apply-seller/apply-seller.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { StoreType } from "@/lib/types";
+import { useState } from "react";
+import Instructions from "./instructions";
+
+export default function ApplySellerMultiForm() {
+    const [step, setStep] = useState<number>(1);
+    const [formData, setFormData] = useState<StoreType>({
+        name: "",
+        description: "",
+        email: "",
+        phone: "",
+        url: "",
+        logo: "",
+        cover: "",
+        defaultShippingService: "",
+        defaultDeliveryTimeMax: undefined,
+        defaultDeliveryTimeMin: undefined,
+        defaultShippingFeeFixed: undefined,
+        defaultShippingFeeForAdditionalItem: undefined,
+        defaultShippingFeePerItem: undefined,
+        defaultShippingFeePerKg: undefined,
+        returnPolicy: "",
+    });
+    return (
+        <div className="grid grid-cols-[400px_1fr]">
+            <Instructions/>
+            <div className="relative w-full p-5">
+                {/* Progress Bar */}
+                {/* Steps */}
+            </div>
+        </div>
+    );
+}
