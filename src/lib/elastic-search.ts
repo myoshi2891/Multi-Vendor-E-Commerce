5c811fcebade1479317e36b4380f1e15174cb45c
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-08-17 20:52:42 +0900

    Remove files and cache related elasticsearch & meilisearch

--- src/lib/elastic-search.ts
+++ src/lib/elastic-search.ts
@@ -1,12 +1,12 @@
-import { Client } from '@elastic/elasticsearch'
+// import { Client } from '@elastic/elasticsearch'
 
-const client = new Client({
-    cloud: {
-        id: process.env.ELASTICSEARCH_CLOUD_ID || '',
-    },
-    auth: {
-        apiKey: process.env.ELASTICSEARCH_API_KEY || '',
-    }
-})
+// const client = new Client({
+//     cloud: {
+//         id: process.env.ELASTICSEARCH_CLOUD_ID || '',
+//     },
+//     auth: {
+//         apiKey: process.env.ELASTICSEARCH_API_KEY || '',
+//     }
+// })
 
-export default client
+// export default client
